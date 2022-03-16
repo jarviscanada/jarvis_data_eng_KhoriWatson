45b869ecc672371f25c50f8793df89ef5cc74a76
Khori Watson <khori.watson@gmail.com>
2022-03-16 21:02:49 +0000

    Create psql_docker script

---
+++ linux_sql/scripts/psql_docker.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#Extract command line arguments
+command=$1
+db_username=$2
+db_password=$3
+
+#Start docker if not started
+sudo systemctl status docker || sudo systemctl start docker
+
+docker container inspect jrvs-psql
+container_status=$?
+
+#Case dependent on command line argument
+case $command in
+  create)
+    #Check if container exists
+    if [ $container_status -eq 0 ]; then
+      echo 'Container already exists'
+      exit 1
+    fi
+    #Check if there are a sufficient number of command line arguments
+    if [ $# -ne 3 ]; then
+      echo 'Create requires a username and password'
+      exit 1
+    fi
+
+    #Create container
+    docker volume create pgdata
+    export PGPASSWORD="$db_password"
+    export PGUSER="$db_username"
+    docker run --name jrvs-psql -e POSTGRES_PASSWORD="$PGPASSWORD" -e POSTGRES_USER="$PGUSER" -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres:9.6-alpine
+    exit $?
+  ;;
+
+  start|stop)
+    #Check if
+    if [ $container_status -ne 0 ]; then
+      echo 'Container does not exist (must create container first)'
+      exit 1
+    fi
+
+    docker container "$command" jrvs-psql
+    exit $?
+  ;;
+
+  *)
+    echo 'Illegal command'
+    echo 'Commands: start|stop|create'
+    exit 1
+  ;;
+
+esac
