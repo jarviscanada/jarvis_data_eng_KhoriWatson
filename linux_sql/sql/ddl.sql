d300dcbefe0853e5651db2d1a5dec65ea8c9f91d
Khori Watson <khori.watson@gmail.com>
2022-03-21 05:05:05 +0000

    Create the host_info script and fix ddl.sql file

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE IF NOT EXISTS host_info (
     id SERIAL NOT NULL PRIMARY KEY,
-    host_id VARCHAR (255) NOT NULL UNIQUE,
+    hostname VARCHAR (255) NOT NULL UNIQUE,
     num_cpu INTEGER NOT NULL,
     cpu_architecture VARCHAR (255) NOT NULL,
     cpu_model VARCHAR (255) NOT NULL,
@@ -14,12 +14,12 @@ CREATE TABLE IF NOT EXISTS host_info (
 
 CREATE TABLE IF NOT EXISTS host_usage (
     "timestamp" TIMESTAMP NOT NULL,
-    host_id VARCHAR (255) NOT NULL UNIQUE,
+    host_id INTEGER NOT NULL UNIQUE,
     memory_free INTEGER NOT NULL,
     cpu_idle INTEGER NOT NULL,
     cpu_kernel INTEGER NOT NULL,
     disk_io INTEGER NOT NULL,
     disk_available INTEGER NOT NULL,
     FOREIGN KEY (host_id)
-        REFERENCES host_info (host_id)
+        REFERENCES host_info (id)
 );
