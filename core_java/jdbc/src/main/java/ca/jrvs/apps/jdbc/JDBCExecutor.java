5c6bdb53d0ad7f7f493fca6cd00141dce88d7b64
Khori Watson <khori.watson@gmail.com>
2022-04-22 12:45:19 -0400

    Implement Order DAO

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -12,28 +12,31 @@ public class JDBCExecutor {
         "postgres", "password");
     try {
       Connection connection = dcm.getConnection();
-      CustomerDAO customerDAO = new CustomerDAO(connection);
-      Customer customer = new Customer();
-
-      customer.setFirstName("John");
-      customer.setLastName("Adams");
-      customer.setEmail("john.adams@wh.gov");
-      customer.setPhone("(555) 555-6543");
-      customer.setAddress("1234 Main St");
-      customer.setCity("Arlington");
-      customer.setState("VA");
-      customer.setZipCode("01234");
-
-      Customer dbCustomer = customerDAO.create(customer);
-      System.out.println(dbCustomer);
-
-      dbCustomer = customerDAO.findById(dbCustomer.getId());
-      System.out.println(dbCustomer);
-
-      dbCustomer.setEmail("jadams@wh.gov");
-      dbCustomer = customerDAO.update(dbCustomer);
-      System.out.println(dbCustomer);
-      customerDAO.delete(dbCustomer.getId());
+      OrderDAO orderDAO = new OrderDAO(connection);
+      Order order = orderDAO.findById(1000);
+      System.out.println(order);
+//      CustomerDAO customerDAO = new CustomerDAO(connection);
+//      Customer customer = new Customer();
+//
+//      customer.setFirstName("John");
+//      customer.setLastName("Adams");
+//      customer.setEmail("john.adams@wh.gov");
+//      customer.setPhone("(555) 555-6543");
+//      customer.setAddress("1234 Main St");
+//      customer.setCity("Arlington");
+//      customer.setState("VA");
+//      customer.setZipCode("01234");
+//
+//      Customer dbCustomer = customerDAO.create(customer);
+//      System.out.println(dbCustomer);
+//
+//      dbCustomer = customerDAO.findById(dbCustomer.getId());
+//      System.out.println(dbCustomer);
+//
+//      dbCustomer.setEmail("jadams@wh.gov");
+//      dbCustomer = customerDAO.update(dbCustomer);
+//      System.out.println(dbCustomer);
+//      customerDAO.delete(dbCustomer.getId());
 
 //      Customer customer = customerDAO.findById(1000);
 //      System.out.println(customer.getFirstName() + " " + customer.getLastName() + " " + customer.getEmail());
